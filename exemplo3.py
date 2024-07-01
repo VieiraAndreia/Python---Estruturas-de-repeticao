167cb0e9d76c76042654da52bb2843387d47c225
Andréia Maria do Nascimento Vieira <160866657+VieiraAndreia@users.noreply.github.com>
2024-07-01 14:32:35 -0300

    Delete exemplo3.py

--- exemplo3.py
+++
@@ -1,9 +0,0 @@
-#exemplo sobre um numero 5 vezes multiplicado por 3
-
-contador=1          #condição começa do 1
-while contador<=5:
-    x=int(input("Digite um número:"))
-    r=x*3
-    print(r)
-    contador+=1     #dá o limite das repetições
-print("Fim do programa!")
