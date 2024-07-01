796c1a4999c4494f6d2a4a8f75cb2e04867f1c5c
Andréia Maria do Nascimento Vieira <160866657+VieiraAndreia@users.noreply.github.com>
2024-07-01 14:32:24 -0300

    Delete exemplo2.py

--- exemplo2.py
+++
@@ -1,5 +0,0 @@
-#Exemplo de condição while verdadeiro, para o while ficar rodando infinitamente até que tenha algum comando para parar
-
-while True:
-    print("Estou preso nesse loop para sempre!")
-    break             #usado para parar o loop
