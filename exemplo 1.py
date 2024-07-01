8f16c14bd9f0576d7a068af078a8e329e12d0588
Andréia Maria do Nascimento Vieira <160866657+VieiraAndreia@users.noreply.github.com>
2024-07-01 14:31:56 -0300

    Delete exemplo 1.py

--- exemplo 1.py
+++
@@ -1,7 +0,0 @@
-# exemplo sobre o operador de repetição WHILE
-#o While atende a uma condição específica para funcionar
-
-contador=0
-while contador<5:    
-    print(contador)
-    contador+=1           # contador= contador+1
